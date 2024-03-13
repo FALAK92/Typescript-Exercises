5846ad30194f8dea067d73bc3c1bf8aa5f3baf17
FALAK92 <160516863+FALAK92@users.noreply.github.com>
2024-03-13 17:22:00 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 "use strict";
 let Guest_List = ['Zoya', 'Zimal', 'Zubia'];
 for (let i = 0; i < Guest_List.length; i++) {
-    console.log('My lovely friend.' + Guest_List[i] + ',\n\n it is my pleasure to invite you in my birthday party.\n\n THANK YOU!');
+    console.log('My lovely friend.' + Guest_List[i] + ',\n\n it is my pleasure to invite you in my birthday party.\n\n THANK YOU!\n\n');
 }
+let absent_Guest = 'Zoya';
+let new_Guest = 'Mishaal';
+Guest_List[0] = new_Guest;
+for (let i = 0; i < Guest_List.length; i++) {
+    console.log('My lovely friend.' + Guest_List[i] + ',\n\n it is my pleasure to invite you in my birthday party.\n\n THANK YOU!\n\n');
+}
+console.log(`${absent_Guest} is not coming in my birthday party.`);
--- app.ts
+++ app.ts
@@ -1,9 +1,24 @@
 
 
-let Guest_List :string[] =['Zoya','Zimal','Zubia'];
 
-for(let i=0; i<Guest_List.length; i++){
+let Guest_List :string[] = ['Zoya', 'Zimal', 'Zubia'];
 
-    console.log('My lovely friend.' + Guest_List[i] + ',\n\n it is my pleasure to invite you in my birthday party.\n\n THANK YOU!');
-    
+for (let i = 0; i < Guest_List.length; i++) {
+
+    console.log('My lovely friend.' + Guest_List[i] + ',\n\n it is my pleasure to invite you in my birthday party.\n\n THANK YOU!\n\n');
+
 }
+
+let absent_Guest: string ='Zoya';
+
+let new_Guest: string ='Mishaal';
+
+Guest_List[0] = new_Guest;
+
+for (let i = 0; i < Guest_List.length; i++) {
+
+    console.log('My lovely friend.' + Guest_List[i] + ',\n\n it is my pleasure to invite you in my birthday party.\n\n THANK YOU!\n\n');
+
+}
+
+console.log(`${absent_Guest} is not coming in my birthday party.`);
