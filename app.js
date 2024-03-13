b507daa81abc4232396ef5a85dce200b1b7c5566
FALAK92 <160516863+FALAK92@users.noreply.github.com>
2024-03-13 17:24:09 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -10,3 +10,10 @@ for (let i = 0; i < Guest_List.length; i++) {
     console.log('My lovely friend.' + Guest_List[i] + ',\n\n it is my pleasure to invite you in my birthday party.\n\n THANK YOU!\n\n');
 }
 console.log(`${absent_Guest} is not coming in my birthday party.`);
+console.log('Good News! I found a big table so i invite 3 more friends.');
+Guest_List.unshift('Suhaima');
+Guest_List.splice(2, 0, 'Iqra');
+Guest_List.push('Sonya');
+for (let i = 0; i < Guest_List.length; i++) {
+    console.log('My lovely friend.' + Guest_List[i] + ',\n\n it is my pleasure to invite you in my birthday party.\n\n THANK YOU!\n\n');
+}
