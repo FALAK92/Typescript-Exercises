bdf0e5cc8569e42d3d52f0e8a2fa956eb944ab55
FALAK92 <160516863+FALAK92@users.noreply.github.com>
2024-03-13 17:17:28 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 "use strict";
 let names = ["Falak", "Anam", "Sana", "Aisha"];
+const message = "welcome to governor initiative program: ";
 for (let i = 0; i < names.length; i++) {
-    console.log(names[i]);
+    console.log(message + names[i]);
 }
--- app.ts
+++ app.ts
@@ -1,9 +1,9 @@
-let names: string[] = ["Falak","Anam","Sana","Aisha"];
+let names :string[] = ["Falak", "Anam", "Sana", "Aisha"];
 
+const message :string = "welcome to governor initiative program: ";
 
+for (let i = 0; i < names.length; i++) {
 
-for(let i=0; i<names.length; i++){
-
-    console.log(names[i]);
+    console.log(message + names[i]);
 
 }
