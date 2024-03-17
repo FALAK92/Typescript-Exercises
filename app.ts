84df84d7a994576589f456fffa41e6d2597d42bd
FALAK92 <160516863+FALAK92@users.noreply.github.com>
2024-03-17 16:06:34 +0500

    Add files via upload

--- app.ts
+++ app.ts
@@ -1,16 +1,15 @@
-// Cities: Write a function called describe_city() that accepts the name of a city and its country.
-//  The function should print a simple sentence, such as Karachi is in Pakistan. 
-//  Give the parameter for the country a default value. Call your function for three different cities,
-//  at least one of which is not in the default country.
+// City Names: Write a function called city_country() that takes in the name of a city and its country.
+//  The function should return a string formatted like this:
+// "Lahore, Pakistan"
+// Call your function with at least three city-country pairs, and print the value that’s returned.
 
-function describe_city(city:string , country:string = 'default country'){
+function city_country(city:string, country:string){
 
-    console.log(`${city} is in ${country}`);
-
+    return`${city}, ${country}`;
 }
 
-describe_city('Karachi', 'Pakistan');
+console.log(city_country('karachi', 'Pakistan'));
 
-describe_city('Jeddah', 'Saudia Arabia');
+console.log(city_country('Tokyo', 'Japan'));
 
-describe_city('Tokyo');
+console.log(city_country('Paris', 'France'));
