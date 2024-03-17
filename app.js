162d4dafb0ccd3cb2d799c7daedd2593708e3b63
FALAK92 <160516863+FALAK92@users.noreply.github.com>
2024-03-17 16:05:09 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 "use strict";
-// T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be
-//  printed on the shirt. The function should print a sentence summarizing the size of the shirt and
-//   the message printed on it.
-//  Call the function.
-function make_shirt(size, text) {
+// Large Shirts: Modify the make_shirt() function so that shirts are large by default with a message
+//  that reads I love TypeScript. Make a large shirt and a medium shirt with the default message,
+//  and a shirt of any size with a different message.
+function make_shirt(size = 'Large', text = 'I love Typescript') {
     console.log(`Creating a ${size} shirt with the message: ${text}`);
 }
-make_shirt(`large`, `Ramzan Kareem`);
+make_shirt();
+make_shirt('Medium');
+make_shirt('Small', 'I love Python');
