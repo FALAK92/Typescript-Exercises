8371a1e7ae154f8c4966acd3324c467d316c8134
FALAK92 <160516863+FALAK92@users.noreply.github.com>
2024-03-17 15:57:56 +0500

    Add files via upload

--- app.ts
+++ app.ts
@@ -1,19 +1,15 @@
-// # Hello Admin: Make a array of five or more usernames, including the name 'admin'.
-// #  Imagine you are writing code that will print a greeting to each user after they log in to a website.
-// #   Loop through the array, and print a greeting to each user:
-// #  If the username is 'admin', print a special greeting,
-// #   such as Hello admin, would you like to see a status report
-// #  Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
+// No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
+//  If the list is empty, print the message We need to find some users!
 
-let userNames: string[] = ['Fahad','Ali','Saif','Admin','Ammar','Jazib'];
+// Remove all of the usernames from your array, and make sure the correct message is printed.
 
-for(let i =0; i<userNames.length; i++)
-{
-    if(userNames[i] === 'Admin'){
+let userNames: string[] = ['Ali','Sana','Zara','Admin','Safa'];
 
-        console.log('Hello Admin, Would you like to see a status report?');
-    }
-    else{
-        console.log(`Hello ${userNames[i]}, thank you for logging in again.`);
-    }
+if(userNames.length === 0){
+
+    console.log('We need to find some users!');
+
+}else{
+    userNames =[];
+    console.log('All users names have been removed. ' + userNames.length);
 }
