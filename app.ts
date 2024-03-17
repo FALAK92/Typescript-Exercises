2be6a6d36dff376cb5fc660f9843f2cbe0474ab8
FALAK92 <160516863+FALAK92@users.noreply.github.com>
2024-03-17 16:02:44 +0500

    Add files via upload

--- app.ts
+++ app.ts
@@ -1,27 +1,16 @@
-// Ordinal Numbers: Ordinal numbers indicate their position in a array,
-//  such as 1st or 2nd. Most ordinal numbers end in th, except 1, 2, and 3.
-// • Store the numbers 1 through 9 in a array.
-// • Loop through the array.
-// • Use an if-else chain inside the loop to print the proper ordinal ending for each number.
-//  Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th",
-//  and each result should be on a separate line.
+// Pizzas: Think of at least three kinds of your favorite pizza.
+//  Store these pizza names in a array, and then use a for loop to print the name of each pizza.
+// • Modify your for loop to print a sentence using the name of the pizza instead of printing just the
+//  name of the pizza. For each pizza you should have one line of output containing 
+//  a simple statement like I like pepperoni pizza.
+// • Add a line at the end of your program, outside the for loop, that states how much you like pizza. 
+// The output should consist of three or more lines about the kinds of pizza you like and then an additional
+//  sentence, such as I really love pizza!
 
-let numbers: number[] = [1,2,3,4,5,6,7,8,9];
+let pizzas: string[] = ['Pepperoni','BBQ Chicken','California'];
 
-for(let num of numbers){
-
-    let ordinalEnding:string;
-    if(num === 1){
-        ordinalEnding = "st";
-    }
-    else if(num === 2){
-        ordinalEnding = "nd";
-    }
-    else if(num === 3){
-        ordinalEnding = "rd";
-    }
-    else{
-        ordinalEnding = "th";
-    }
-    console.log(`${num}${ordinalEnding}`);
+for(let pizza of pizzas){
+    console.log(`I like ${pizza} pizza.`);
 }
+
+console.log(`I really love pizza!`);
