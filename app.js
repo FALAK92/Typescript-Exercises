3aa7b142bde26cc05b2560070f67901a44dd71c8
FALAK92 <160516863+FALAK92@users.noreply.github.com>
2024-03-17 16:01:54 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 "use strict";
-// Checking Usernames: Do the following to create a program that simulates how websites ensure that everyone
-//  has a unique username.
-// • Make a list of five or more usernames called current_users.
-// • Make another list of five usernames called new_users.
-//  Make sure one or two of the new usernames are also in the current_users list.
-// • Loop through the new_users list to see if each new username has already been used.
-//  If it has, print a message that the person will need to enter a new username. 
-//  If a username has not been used, print a message saying that the username is available.
-// • Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted.
-let current_users = ['TaHa', 'DaNiyal', 'HiRa', 'Saba', 'John'];
-let new_users = ['Fidyan', 'Zarnab', 'TaHa', 'Sehar', 'Saba'];
-new_users.forEach(newusernames => {
-    let lowercase = newusernames.toLowerCase();
-    if (current_users.map(c_user => c_user.toLowerCase()).includes(lowercase)) {
-        console.log(`The username ${newusernames} is not available. please write a different username`);
+// Ordinal Numbers: Ordinal numbers indicate their position in a array,
+//  such as 1st or 2nd. Most ordinal numbers end in th, except 1, 2, and 3.
+// • Store the numbers 1 through 9 in a array.
+// • Loop through the array.
+// • Use an if-else chain inside the loop to print the proper ordinal ending for each number.
+//  Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th",
+//  and each result should be on a separate line.
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (let num of numbers) {
+    let ordinalEnding;
+    if (num === 1) {
+        ordinalEnding = "st";
+    }
+    else if (num === 2) {
+        ordinalEnding = "nd";
+    }
+    else if (num === 3) {
+        ordinalEnding = "rd";
     }
     else {
-        console.log(`The username ${newusernames} is available`);
+        ordinalEnding = "th";
     }
-});
+    console.log(`${num}${ordinalEnding}`);
+}
