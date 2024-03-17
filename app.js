34fb7d6b1aa2d9fea087103786dfafc43acc0d11
FALAK92 <160516863+FALAK92@users.noreply.github.com>
2024-03-17 16:07:16 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 "use strict";
-// City Names: Write a function called city_country() that takes in the name of a city and its country.
-//  The function should return a string formatted like this:
-// "Lahore, Pakistan"
-// Call your function with at least three city-country pairs, and print the value that’s returned.
-function city_country(city, country) {
-    return `${city}, ${country}`;
+// Album: Write a function called make_album() that builds a Object describing a music album.
+//  The function should take in an artist name and an album title, 
+//  and it should return a Object containing these two pieces of information. 
+//  Use the function to make three dictionaries representing different albums. 
+//  Print each return value to show that Objects are storing the album information correctly. 
+//  Add an optional parameter to make_album() that allows you to store the number of tracks on an album.
+//   If the calling line includes a value for the number of tracks, add that value to the album’s Object.
+//  Make at least one new function call that includes the number of tracks on an album.
+function make_album(artist, title, tracks) {
+    const album = {
+        artist: artist,
+        title: title,
+    };
+    if (tracks !== undefined) {
+        album.tracks = tracks;
+    }
+    return album;
 }
-console.log(city_country('karachi', 'Pakistan'));
-console.log(city_country('Tokyo', 'Japan'));
-console.log(city_country('Paris', 'France'));
+const album1 = make_album('Artist 1', 'Album title 1');
+console.log(album1);
+const album2 = make_album('Artist 2', 'Album title 2');
+console.log(album2);
+const album3 = make_album('Artist 3', 'Album title 3', 12);
+console.log(album3);
