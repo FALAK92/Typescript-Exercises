8eec661f7699fbcb6f7b2c6d8167f8ec2dd61d73
FALAK92 <160516863+FALAK92@users.noreply.github.com>
2024-03-17 15:56:16 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
 "use strict";
-// Stages of Life: Write an if-else chain that determines a person’s stage of life. 
-// Set a value for the variable age, and then:
-let age = 18;
-// • If the person is less than 2 years old, print a message that the person is a baby.
-// • If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
-// • If the person is at least 4 years old but less than 13, print a message that the person is a kid.
-// • If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
-// • If the person is at least 20 years old but less than 65, print a message that the person is an adult.
-// • If the person is age 65 or older, print a message that the person is an elder.
-if (age < 2) {
-    console.log("person is a baby");
+// Favorite Fruit: Make a array of your favorite fruits,
+//  and then write a series of independent if statements that check for certain fruits in your array.
+// Make a array of your three favorite fruits and call it favorite_fruits.
+// Write five if statements. Each should check whether a certain kind of fruit is in your array. 
+// If the fruit is in your array, the if block should print a statement, such as You really like bananas!
+let favorite_fruits = ['Apple', 'Banana', 'Orange'];
+if (favorite_fruits.includes('Apple')) {
+    console.log("I really like Apple!");
 }
-else if (age >= 2 && age < 4) {
-    console.log("person is a toddler");
+if (favorite_fruits.includes('Banana')) {
+    console.log("I really like Banana!");
 }
-else if (age >= 4 && age < 13) {
-    console.log("person is a kid");
+if (favorite_fruits.includes('Orange')) {
+    console.log("I really like Orange!");
 }
-else if (age >= 13 && age < 20) {
-    console.log("person is a teenager");
+if (favorite_fruits.includes('Grapes')) {
+    console.log("I really like Grapes!");
 }
-else if (age >= 20 && age < 65) {
-    console.log("person is an adult");
-}
-else {
-    console.log("person is an elder");
+if (favorite_fruits.includes('Mango')) {
+    console.log("I really like Mango!");
 }
