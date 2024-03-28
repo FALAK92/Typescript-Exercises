df77e75cbd8969ebfd161861ddce1873acb7f0a3
FALAK92 <160516863+FALAK92@users.noreply.github.com>
2024-03-28 21:15:28 +0500

    Add files via upload

--- app.ts
+++ app.ts
@@ -1,17 +1,22 @@
-// Sandwiches: Write a function that accepts a array of items a person wants on a sandwich.
-//  The function should have one parameter that collects as many items as the function call provides,
-//   and it should print a summary of the sandwich that is being ordered. Call the function three times, 
-//   using a different number of arguments each time.
+// Cars: Write a function that stores information about a car in a Object.
+//  The function should always receive a manufacturer and a model name.
+//  It should then accept an arbitrary number of keyword arguments.
+//  Call the function with the required information and two other name-value pairs,
+//  such as a color or an optional feature. Print the Object that’s returned to make sure all
 
-function makeSandwich (item:string[]){
-    console.log('\nMaking your sandwich with:');
+import { features } from "process";
 
-    item.forEach(element => console.log('- ' + element));
+//   the information was stored correctly.
+function storeCarInfo(manufacturer:string, modelName:string, ...extraOption:{ [key:string] :any} []):
+object{
+     
+    const carInfo={
+        manufacturer,
+        modelName,
+       ... Object.assign({}, ...extraOption)
+    }
+    return carInfo;
+};
 
-    console.log('Enjoy your sandwich!\n');
-}
- makeSandwich(['Ham', 'Cheese', 'Lettuce']);
-
- makeSandwich(['Turkey', 'Bacon']);
-
- makeSandwich(['Peanut butter', 'Jelly']);
+let answer = storeCarInfo('Honda', 'civic', {color:'Black'},{features:['Navigation', 'Power window']})
+console.log(answer);
