8d075ca24cd7e1e367f242627e984f390fd07106
FALAK92 <160516863+FALAK92@users.noreply.github.com>
2024-03-28 21:12:11 +0500

    Add files via upload

--- app.ts
+++ app.ts
@@ -1,13 +1,25 @@
 
-// Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(),
-//  which prints the name of each magician in the array.
+// Great Magicians: Start with a copy of your program from Exercise 39. 
+// Write a function called make_great() that modifies the array of magicians by adding the 
+// phrase the Great to each magician’s name. Call show_magicians() to see that the list has 
+// actually been modified.
+
+
 
 let magician: string[] = ['Harry potter','Hermione Granger','Ron Weasley','Albus Dumbledore'];
 
+function make_great(magicianarray:string[]){
+
+    for(let i=0; i<magicianarray.length; i++){
+
+    magician[i] = 'The Great ' + magicianarray[i]
+    }
+}
 function show_magicians(Magicians: string[]){
 
     Magicians.forEach(element => {
         console.log(element);
     });
 }
+make_great(magician);
 show_magicians(magician);
