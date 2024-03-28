7e1e214ff3cdfd1bf0951b7f632a187e7a6951af
FALAK92 <160516863+FALAK92@users.noreply.github.com>
2024-03-28 21:11:13 +0500

    Add files via upload

--- app.ts
+++ app.ts
@@ -1,29 +1,13 @@
-// Album: Write a function called make_album() that builds a Object describing a music album.
-//  The function should take in an artist name and an album title, 
-//  and it should return a Object containing these two pieces of information. 
-//  Use the function to make three dictionaries representing different albums. 
-//  Print each return value to show that Objects are storing the album information correctly. 
-//  Add an optional parameter to make_album() that allows you to store the number of tracks on an album.
-//   If the calling line includes a value for the number of tracks, add that value to the album’s Object.
-//  Make at least one new function call that includes the number of tracks on an album.
 
-function make_album(artist: string, title: string, tracks?:number) {
+// Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(),
+//  which prints the name of each magician in the array.
 
-    const album: {artist: string, title: string, tracks?:number} = {
-        artist : artist,
-        title :title,
-    }
+let magician: string[] = ['Harry potter','Hermione Granger','Ron Weasley','Albus Dumbledore'];
 
-    if(tracks !== undefined){
-        album.tracks = tracks;
-    }
-    return album;
+function show_magicians(Magicians: string[]){
+
+    Magicians.forEach(element => {
+        console.log(element);
+    });
 }
- const album1 = make_album('Artist 1','Album title 1');
- console.log(album1);
-
- const album2 = make_album('Artist 2','Album title 2');
- console.log(album2);
-
- const album3 = make_album('Artist 3','Album title 3' , 12);
- console.log(album3);
+show_magicians(magician);
